1b7bf602dacaed304ccd4ec4516738eeb7fffa8a
pygamer80 <pygamer80@gmail.com>
2022-06-27 08:52:23 -0300

    finalizado

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,63 +1,47 @@
-function capturar(){
-    let pow = document.getElementById("texto1").value;
-    let tension = document.getElementById("texto2").value;
-    let amper = document.getElementById("texto3").value;
-    let resist = document.getElementById("texto4").value;
-    let w = "";
-    let t = "";
-    let a = "";
-    let r = "";
-    // Calculo potência
-    if(pow.value == undefined && tension.value == undefined){
-        w = parseInt(resist) * Math.pow(parseInt(amper), 2);
-        pow = document.getElementById("texto1").value = w;
-        // Calculo tensão
-        t = parseInt(resist) * parseInt(amper);
-        tension = document.getElementById("texto2").value = t;
-    }
-    if(pow.value == undefined && amper.value == undefined){
-        w = Math.pow(parseInt(tension),2) / parseInt(resist);
-        pow = document.getElementById("texto1").value = w;
-    }
-    if(pow.value == undefined && resist.value == undefined){
-        w = parseInt(tension) * parseInt(amper);
-        pow = document.getElementById("texto1").value = w;
-    } 
-    // Calculo de tensão
-    if(tension.value == undefined && amper.value == undefined){
-        t = mat.sqrt(parseInt(pow) * parseInt(resist));
-        tension = document.getElementById("texto4").value = t;
-    }/*
-    if(tension.value == undefined && resist == undefined){
-        t = parseInt(pow) / parseInt(amper);
-        tension = document.getElementById("texto4").value = t;
-    }
-    // Calculo corrente
-    if(amper.value == undefined && tension.value == undefined){
-        a = Math.sqrt(parseInt(pow) / parseInt(resist));
-        amper = document.getElementById("texto3").value = a;
-    }
-    if(amper.value == undefined && resist.value == undefined){
-        a = parseInt(pow) / parseInt(tension);
-        amper = document.getElementById("texto3").value = a;
-    }
-    if(amper.value == undefined && pow.value == undefined){
-        a = parseInt(tension) / parseInt(resist);
-        amper = document.getElementById("texto3").value = a;
-    }
-    // Calculo de resistencia
-    if(resist.value == undefined && pow == undefined){
-        r = parseInt(tension) / parseInt(amper);
-        resist = document.getElementById("texto4").value = r;
-    }
-    if(resist.value == undefined && tension == undefined){
-        r = parseInt(pow) / Math.pow(parseInt(amper), 2);
-        resist = document.getElementById("texto4").value = r;
-    }
-    if(resist.value == undefined && amper == undefined){
-        r = math.pow(parseInt(tension), 2) / parseInt(pow);
-        resist = document.getElementById("texto4").value = r;
-    }*/
-    console.log(pow, tension, amper, resist, w, t, a, r)
+function calcular(){
     
-} 
+    var pow = document.getElementById("texto1").value;
+    var tension = document.getElementById("texto2").value;
+    var corrent = document.getElementById("texto3").value;
+    var resist = document.getElementById("texto4").value;
+    var w = parseInt(pow);
+    var t = parseInt(tension);
+    var i = parseInt(corrent);
+    var r = parseInt(resist);
+    console.log(w, t, i, r)
+
+    if(isNaN(w) && isNaN(t)){
+        w = r * Math.pow(i, 2) 
+        t = r * i
+    }
+    else{
+    if(isNaN(w) && isNaN(i)){
+        w = Math.pow(t, 2)/ r
+        i = t / r
+        }
+    else{
+        if(isNaN(w) && isNaN(r)){
+        w = i * t
+        r = t / i
+    }
+    else{
+        if(isNaN(t) && isNaN(i)){
+        t = Math.sqrt(w * r) 
+        i = Math.sqrt(w / r)
+    }
+    else{
+        if(isNaN(i) && isNaN(r)){
+        i = w / t
+        r = Math.pow(t, 2) / w
+    }
+    }
+    }
+    }
+    }
+
+    document.getElementById("texto1").value = w
+    document.getElementById("texto2").value = t
+    document.getElementById("texto3").value = i
+    document.getElementById("texto4").value = r
+    console.log(w, t, i, r)
+}
